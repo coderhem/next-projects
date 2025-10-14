a86a292fb92e712a30b19d4a43cd3a4ae085de8d
hemraj <hj130640@gmail.com>
2025-10-14 08:22:01 +0545

    Form success message and style updated

--- src/app/membership/page.js
+++ src/app/membership/page.js
@@ -1,63 +1,200 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const Membership = () => {
- return (
-  <>
+   const bookSchema = z.object({
+      fullName: z
+         .string({ required_error: "Name is required*" })
+         .min(1, "Name is required*")
+         .refine((val) => val !== null && val !== "", {
+            message: "Name is required*",
+         }),
 
-   <section className='py-10 bg-primary/5'>
-    <div className="container">
-     <div className="text-center max-w-3xl mx-auto">
-      <h2 className='h4 text-black mb-2'><span className='text-primary'>Get</span> Membership</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At dolorum quas ipsam libero eaque voluptates mollitia quo, consequatur sunt enim voluptas, iste magni. Obcaecati culpa cum exercitationem officiis ad fuga?</p>
-     </div>
-     <div className="overflow-hidden membership-form pt-10 max-w-3xl shadow-lg mx-auto p-10 rounded-lg relative before:absolute before:size-20 before:bg-primary before:rounded-full  before:-top-10 before:-left-10 before:blur-2xl after:absolute after:size-20 after:bg-primary after:rounded-full after:bottom-0 after:right-0 after:blur-3xl after:z-[-1] border border-primary/20">
-      <form action="" className='flex flex-wrap'>
-       <div className="form-group !w-full">
-        <input type="text" name="full-name" placeholder='Full Name' id="fullName" className='form-control' />
-       </div>
-       <div className="form-group">
-        <input type="text" name="phone" placeholder='Phone Number' id="number" className='form-control' />
-       </div>
-       <div className="form-group">
-        <input type="email" name="email" placeholder='Email Address' id="email" className='form-control' />
-       </div>
-       <div className="form-group">
-        <select name="" id="" className='form-control'>
-         <option value="Gender">Gender</option>
-         <option value="Male">Male</option>
-         <option value="Female">Female</option>
-         <option value="Others">Others</option>
-        </select>
-       </div>
-       <div className="form-group">
-        <select name="" id="" className='form-control'>
-         <option value="O+ve">Blood Group</option>
-         <option value="O+ve">O<sup>+ve</sup></option>
-         <option value="O+ve">A<sup>+ve</sup></option>
-         <option value="O+ve">B<sup>+ve</sup></option>
-         <option value="O+ve">AB<sup>+ve</sup></option>
-         <option value="O+ve">A<sup>-ve</sup></option>
-         <option value="O+ve">AB<sup>-ve</sup></option>
-         <option value="O+ve">O<sup>-ve</sup></option>
+      email: z.string().email("Email is required*").optional(),
 
-        </select>
-       </div>
-       <div className="form-group !w-full">
-        <input type="text" name="address" placeholder='Address' id="addressDetail" className='form-control' />
-       </div>
-       <div className="px-3 w-full">
-        <button className='btn btn-green px-3 w-full' type='submit'>Submit</button>
-       </div>
-      </form>
-         <div className="pt-4 text-center">
-         <p>If you have an membership <a href='/login' className='text-primary !underline hover:!no-underline'>Login Here</a></p>
-        </div>
-     </div>
-    </div>
-   </section>
+      phone: z
+         .string()
+         .refine((val) => val !== null && val !== "", {
+            message: "Phone number is required*",
+         })
+         .regex(/^\d{10}$/, "Number must be 10 digits"),
 
-  </>
- )
-}
+      genderSelect: z.string().refine((val) => val !== "", {
+         message: "Please select a Gender",
+      }),
+      bloodGroupSelect: z.string().refine((val) => val !== "", {
+         message: "Please select a Blood Group",
+      }),
+   });
+
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      reset,
+   } = useForm({
+      resolver: zodResolver(bookSchema),
+   });
+
+
+   const [successMsg, setSuccessMsg] = useState(""); // success message state
+
+   const onSubmit = (data) => {
+      console.log("Form Submitted:", data);
+      reset(); // reset form
+      setSuccessMsg("Your membership request has been submitted successfully!"); // set success message
+      setTimeout(() => setSuccessMsg(""), 5000); // remove message after 5s
+   };
+
+   return (
+      <>
+         <section className="py-10 bg-primary/5">
+            <div className="container">
+               <div className="text-center max-w-3xl mx-auto">
+                  <h2 className="h4 text-black mb-2">
+                     <span className="text-primary">Get</span> Membership
+                  </h2>
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                     dolorum quas ipsam libero eaque voluptates mollitia quo,
+                     consequatur sunt enim voluptas, iste magni. Obcaecati culpa cum
+                     exercitationem officiis ad fuga?
+                  </p>
+               </div>
+               <div className="overflow-hidden membership-form pt-10 max-w-3xl shadow-lg mx-auto p-10 rounded-lg relative before:absolute before:size-20 before:bg-primary before:rounded-full before:-top-10 before:-left-10 before:blur-2xl after:absolute after:size-20 after:bg-primary after:rounded-full after:bottom-0 after:right-0 after:blur-3xl after:z-[-1] border border-primary/20">
+                  <form
+                     onSubmit={handleSubmit(onSubmit)}
+                     className="flex flex-wrap"
+                     action=""
+                  >
+                     <div className="form-group !w-full">
+                        <input
+                           type="text"
+                           placeholder="Full Name"
+                           id="fullName"
+                           className="form-control"
+                           {...register("fullName")}
+                        />
+                        {errors?.fullName && (
+                           <p className="text-red-700 text-xs pt-2">
+                              {errors.fullName.message}
+                           </p>
+                        )}
+                     </div>
+
+                     <div className="form-group">
+                        <input
+                           type="text"
+                           placeholder="Phone Number"
+                           id="number"
+                           className="form-control"
+                           {...register("phone")}
+                        />
+                        {errors?.phone && (
+                           <p className="text-red-700 text-xs pt-2">
+                              {errors.phone.message}
+                           </p>
+                        )}
+                     </div>
+
+                     <div className="form-group">
+                        <input
+                           type="email"
+                           placeholder="Email Address"
+                           id="email"
+                           className="form-control"
+                           {...register("email")}
+                        />
+                        {errors?.email && (
+                           <p className="text-red-700 text-xs pt-2">
+                              {errors.email.message}
+                           </p>
+                        )}
+                     </div>
+
+                     <div className="form-group">
+                        <select
+                           id="genderSelect"
+                           className="form-control"
+                           {...register("genderSelect")}
+                        >
+                           <option value="">Select Gender</option>
+                           <option value="Male">Male</option>
+                           <option value="Female">Female</option>
+                           <option value="Others">Others</option>
+                        </select>
+                        {errors?.genderSelect && (
+                           <p className="text-red-700 text-xs pt-2">
+                              {errors.genderSelect.message}
+                           </p>
+                        )}
+                     </div>
+
+                     <div className="form-group">
+                        <select
+                           id="bloodGroupSelect"
+                           className="form-control"
+                           {...register("bloodGroupSelect")}
+                        >
+                           <option value="">Blood Group</option>
+                           <option value="O+ve">O+ve</option>
+                           <option value="A+ve">A+ve</option>
+                           <option value="B+ve">B+ve</option>
+                           <option value="AB+ve">AB+ve</option>
+                           <option value="O-ve">O-ve</option>
+                           <option value="A-ve">A-ve</option>
+                           <option value="B-ve">B-ve</option>
+                           <option value="AB-ve">AB-ve</option>
+                        </select>
+                        {errors?.bloodGroupSelect && (
+                           <p className="text-red-700 text-xs pt-2">
+                              {errors.bloodGroupSelect.message}
+                           </p>
+                        )}
+                     </div>
+                     <div className="form-group !w-full">
+                        <input
+                           type="text"
+                           placeholder="Address"
+                           id="addressDetail"
+                           className="form-control"
+                           {...register("address")}
+                        />
+                     </div>
+
+                     <div className="px-3 w-full">
+                        <button className="btn btn-green px-3 w-full" type="submit">
+                           Submit
+                        </button>
+                     </div>
+                  </form>
+                  <div className="pt-4 text-center">
+                     <p>
+                        If you have a membership{" "}
+                        <a
+                           href="/login"
+                           className="text-primary !underline hover:!no-underline"
+                        >
+                           Login Here
+                        </a>
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </section>
+         {/* ✅ Success Message */}
+         {successMsg && (
+            <div className="success-msg">
+               <p className="text-green-600 text-center pt-4 font-semibold">
+                  {successMsg}
+               </p>
+            </div>
+         )}
+      </>
+   );
+};
 
 export default Membership;
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,118 +1,176 @@
 'use client';
+
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 import { z } from "zod";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
 
+  // Zod schema
   const bookSchema = z.object({
     fullName: z
       .string({ required_error: "Name is required*" })
-      .min(1, "Name is required*")
-      .refine((val) => val !== null && val !== '', {
-        message: "Name is required*",
-      }),
-
-    email: z.string().email("Email is required*").optional(),
-
+      .min(1, "Name is required*"),
+    email: z.string().email("Invalid email").optional(),
     phone: z
       .string()
-      .refine((val) => val !== null && val !== '', {
-        message: "Phone Number is required*",
-      })
-      .regex(/^\d{10}$/, "Number must be 10 digits"),
-
+      .min(10, "Number must be 10 digits")
+      .max(10, "Number must be 10 digits")
+      .refine((val) => /^\d{10}$/.test(val), {
+        message: "Number must be 10 digits",
+      }),
     serviceSelect: z.string().refine((val) => val !== "", {
-      message: "Please select a service"
+      message: "Please select a service",
     }),
     doctorSelect: z.string().refine((val) => val !== "", {
-      message: "Please select a doctor"
+      message: "Please select a doctor",
     }),
-    clinicSelect: z
-      .string({ required_error: "Please select a clinic" })
-      .min(1, "Please select a service"),
-  })
+    clinicSelect: z.string().refine((val) => val !== "", {
+      message: "Please select a clinic",
+    }),
+    message: z.string().optional(),
+  });
 
-  const { register, handleSubmit, formState: { errors }, reset } = useForm(
-    {
-      resolver: zodResolver(bookSchema)
-    }
-  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(bookSchema),
+  });
+
   const onsubmit = (data) => {
     console.log(data);
-  }
+    setSubmitted(true); // show thank you message
+    reset(); // reset form
+  };
 
   return (
-    <>
-      <div className="bg-white shadow-[0px_0px_15px_0_rgba(0,191,165,0.2 pt-9 pb-5 px-8">
-        <div className="mb-6 text-black">
-          <h2>Still <span className='text-primary'>Have Questions?</span></h2>
-        </div>
-        <div className="">
-          <form onSubmit={handleSubmit(onsubmit)} className='form'>
-            <div className="form-group !w-full">
-              <input type="text" name='fullName' placeholder='Full Name' className="form-control"
-                {...register("fullName")}
-              />
-              {errors?.fullName && <p className='text-red-700 text-xs pt-2'>{`${errors.fullName.message}`}</p>}
-
-            </div>
-            <div className="form-group">
-              <input type="email" className='form-control' name='email' placeholder='Email'
-                {...register("email")}
-              />
-              {errors?.email && <p className='text-red-700 text-xs pt-2'>{`${errors.email.message}`}</p>}
-
-            </div>
-            <div className="form-group">
-              <input type="text" className='form-control' name='phone' placeholder='Phone'
-                {...register("phone")}
-              />
-              {errors?.phone && <p className='text-red-700 text-xs pt-2'>{`${errors.phone.message}`}</p>}
-
-            </div>
-            <div className="form-group !w-full">
-              <select {...register("serviceSelect")} name="" id="" className='form-control'>
-                <option value="" disabled>Services</option>
-                <option value="">Services</option>
-                <option value="">Services</option>
-              </select>
-              {errors?.serviceSelect && <p className='text-red-700 text-xs pt-2'>{`${errors.serviceSelect.message}`}</p>}
-            </div>
-
-            <div className="form-group">
-              <select  {...register("doctorSelect")} name="" id="" className='form-control'>
-                <option value="" disabled>Doctor</option>
-                <option value="">Services</option>
-                <option value="">Services</option>
-              </select>
-              {errors?.doctorSelect && <p className='text-red-700 text-xs pt-2'>{`${errors.doctorSelect.message}`}</p>}
-
-            </div>
-            <div className="form-group">
-              <select {...register("clinicSelect")} name="" id="" className='form-control'>
-                <option value="Clinic 1" disabled>Clinic</option>
-                <option value="Clinic 2">Services</option>
-                <option value="Clinic 3">Services</option>
-              </select>
-              {errors?.clinicSelect &&
-                <p className='text-red-700 text-xs pt-2'>
-                  {`${errors.clinicSelect.message}`}
-                </p>}
-            </div>
-            <div className="form-group !w-full">
-              <textarea name="" id="" className='form-control !h-20'></textarea>
-            </div>
-            <div className="w-full">
-              <button type='submit' className='bg-primary border-primary btn btn-secondary !w-full'>
-                Submit Now
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className="bg-white shadow-[0px_0px_15px_0_rgba(0,191,165,0.2)] pt-9 pb-5 px-8">
+      <div className="mb-6 text-black">
+        <h2>
+          Still <span className="text-primary">Have Questions?</span>
+        </h2>
       </div>
-    </>
-  )
-}
+
+      {submitted ? (
+        <div className="text-green-600 text-lg font-semibold">
+          Thank you! Your form has been submitted successfully.
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit(onsubmit)} className="form">
+          {/* Full Name */}
+          <div className="form-group !w-full">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="form-control"
+              {...register("fullName")}
+            />
+            {errors.fullName && (
+              <p className="text-red-700 text-xs pt-2">{errors.fullName.message}</p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email"
+              className="form-control"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-700 text-xs pt-2">{errors.email.message}</p>
+            )}
+          </div>
+
+          {/* Phone */}
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Phone"
+              className="form-control"
+              {...register("phone")}
+            />
+            {errors.phone && (
+              <p className="text-red-700 text-xs pt-2">{errors.phone.message}</p>
+            )}
+          </div>
+
+          {/* Service Select */}
+          <div className="form-group !w-full">
+            <select {...register("serviceSelect")} id="serviceSelect" className="form-control">
+              <option value="">Select Services</option>
+              <option value="inquiry">Inquiry</option>
+              <option value="consultation">Consultation</option>
+              <option value="lab_tests">Lab Tests</option>
+              <option value="radiology">Radiology</option>
+              <option value="surgery">Surgery</option>
+              <option value="pharmacy">Pharmacy</option>
+            </select>
+            {errors.serviceSelect && (
+              <p className="text-red-700 text-xs pt-2">{errors.serviceSelect.message}</p>
+            )}
+          </div>
+
+          {/* Doctor Select */}
+          <div className="form-group">
+            <select {...register("doctorSelect")} id="doctorSelect" className="form-control">
+              <option value="">Select Doctor</option>
+              <option value="dr_raj">Dr. Raj Sharma</option>
+              <option value="dr_sita">Dr. Sita Joshi</option>
+              <option value="dr_hem">Dr. Hem Joshi</option>
+              <option value="dr_anil">Dr. Anil Maharjan</option>
+            </select>
+            {errors.doctorSelect && (
+              <p className="text-red-700 text-xs pt-2">{errors.doctorSelect.message}</p>
+            )}
+          </div>
+
+          {/* Clinic Select */}
+          <div className="form-group">
+            <select {...register("clinicSelect")} id="clinicSelect" className="form-control">
+              <option value="">Select Clinic</option>
+              <option value="general_clinic">General Clinic</option>
+              <option value="pediatrics_clinic">Pediatrics Clinic</option>
+              <option value="dermatology_clinic">Dermatology Clinic</option>
+              <option value="cardiology_clinic">Cardiology Clinic</option>
+              <option value="neurology_clinic">Neurology Clinic</option>
+              <option value="orthopedic_clinic">Orthopedic Clinic</option>
+              <option value="radiology_clinic">Radiology Clinic</option>
+              <option value="dental_clinic">Dental Clinic</option>
+              <option value="physiotherapy_clinic">Physiotherapy Clinic</option>
+              <option value="lab_services">Lab Services</option>
+              <option value="pharmacy_services">Pharmacy Services</option>
+            </select>
+            {errors.clinicSelect && (
+              <p className="text-red-700 text-xs pt-2">{errors.clinicSelect.message}</p>
+            )}
+          </div>
+
+          {/* Message */}
+          <div className="form-group !w-full">
+            <textarea
+              placeholder="Message"
+              className="form-control !h-20"
+              {...register("message")}
+            />
+          </div>
+
+          {/* Submit Button */}
+          <div className="w-full">
+            <button type="submit" className="bg-primary border-primary btn btn-secondary !w-full">
+              Submit Now
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
 
 export default ContactForm;
